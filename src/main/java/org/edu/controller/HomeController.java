13067f909fdf72874819b4f9fec069f8d0ec8390
User <User@DESKTOP-6223J9B>
2020-11-27 11:42:35 +0900

    3교시 중간 커밋

--- src/main/java/org/edu/controller/HomeController.java
+++ src/main/java/org/edu/controller/HomeController.java
@@ -26,13 +26,18 @@ public class HomeController {
 	 * 그래서, 저장시 콘솔에 보시면, Reloading Context with... is completed 재실행 되었다고 나옵니다.
 	 * 그래서, .java 클래스는 수정 후 약간 기다린 후 Reloading... 메시지 후 결과 확인이 가능합니다.
 	 */
-	@RequestMapping(value="/",method=RequestMethod.GET)
+	@RequestMapping(value="/",method=RequestMethod.GET)//여기가 절대경로
 	public String index() { //메서드(함수)구성: 리턴(반환값출력)형태지정 메서드명(매개변수) {구현내용} String index(){}
 		//해석:상단 리퀘스트매핑에서 지정한 경로로 접근을 하게되면, 아래에 있는 index()메서드를 실행해라.
 		//메서드명 제일 앞에있는 public는 접근권한에 대한 키워드인데요, public(공공-다른 클래스에서 접근가능)
 		//, private(내부-현재 HomeController클래스에서만 접근가능)
-		//리턴반환값으로 사용한 String 클래스는 문자열을 반환하고, 여기서는 index.jsp와 매칭되는 문자열을 반환.
-		return "";
+		// public String index() { } 자바(스프링) 메서드 기본형태
+		// $(document).ready(function(){ }); j쿼리 기본형태
+		//리턴반환값으로 사용한 String 클래스는 문자열을 반환하고, 
+		//여기서는 index.jsp와 매칭되는 문자열을 반환. 상대경로가 필요합니다.
+		//스프링 MVC프로젝트에서 상대경로의 최상위는 views폴더 최상위 root폴더 입니다.
+		// 접두사(prefix), 접미사(suffix) index(접두사-파일명).jsp(접미사-확장자)
+		return "sample/index";//접두사만 반환값으로 넣습니다. 상대경로.
 	}
 	/*@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
