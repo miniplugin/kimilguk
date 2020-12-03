c929ad961eedc12705b81f4a8cebaed708189c58
User <User@DESKTOP-6223J9B>
2020-12-03 12:51:03 +0900

    4교시 커밋 입니다.

--- src/main/java/org/edu/controller/AdminController.java
+++ src/main/java/org/edu/controller/AdminController.java
@@ -1,8 +1,10 @@
 package org.edu.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 //스프링에서 사용가능한 클래스를 빈(커피Bean)이라고 하고, @Contorller 클래스를 사용하면 됨.
 @Controller
@@ -25,8 +27,16 @@ public class AdminController {
 		return "admin/member/member_write";
 	}
 	
+	//member_list.jsp에서 보낸 데이터를 수신하는 역할 @RequestParam("키이름") 리퀘스트파라미터 클래스 사용.
+	//현재컨트롤러 클래스에서 member_view.jsp로 데이터를 보내는 역할 Model 클래스 사용.
+	//member_list.jsp -> @RequestParam("user_id")수신, Model송신 -> member_view.jsp
 	@RequestMapping(value="/admin/member/member_view",method=RequestMethod.GET)
-	public String member_view() {
+	public String member_view(@RequestParam("user_id") String user_id, Model model) {
+		//내가 멤버 리스트가 보고 싶어서 리스트 클릭한거 자체가 "1명멤버내용 주세요"하고 요청을 한거고 
+		//그 요청을 컨트롤러에서 받은건가요?
+		//위에서 수신한 user_id를 개발자가 만든 user_id2이름으로 member_view.jsp 보냅니다.(아래)
+		//member_view.jsp에서 model로 수신한 데이터 user_id2 를 출력하는 방법은 점심 이후에...
+		model.addAttribute("user_id2", user_id);
 		return "admin/member/member_view";
 	}
 	
