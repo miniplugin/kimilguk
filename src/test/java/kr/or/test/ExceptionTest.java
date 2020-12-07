8144934cea30685c5fcbc953ca7cd5bf1fdbffa4
User <User@DESKTOP-6223J9B>
2020-12-07 10:57:54 +0900

    2교시 커밋 입니다.

---
+++ src/test/java/kr/or/test/ExceptionTest.java
@@ -0,0 +1,12 @@
+package kr.or.test;
+
+public class ExceptionTest {
+
+	public static void main(String[] args) {
+		// 진입(실행)메서드 구현내용
+		int b=0;
+		b=System.in.read();
+		System.out.println("키보드로 입력받은 내용 b = " + b);
+	}
+
+}
