0fe61c4000ad29fe6682a0b217732694c6913bcf
User <User@DESKTOP-6223J9B>
2020-12-07 16:08:32 +0900

    7교시 중간 커밋 입니다.

--- src/test/java/kr/or/test/ThreadApp.java
+++ src/test/java/kr/or/test/ThreadApp.java
@@ -1,15 +1,27 @@
 package kr.or.test;
 
-public class ThreadApp extends Thread {
+public class ThreadApp {
 
-	public void run() {
-		System.out.println("스레드 시작시 자동실행됩니다.!");
-	}
 	public static void main(String[] args) {
 		// 메인스레드 시작 진입점 main()
 		//(new ThreadApp()).start();//아래 2줄과 이곳 1줄과 같습니다.
-		ThreadApp threadApp = new ThreadApp();
-		threadApp.start();
+		MyThread myThread1 = new MyThread("myThread1");
+		MyThread myThread2 = new MyThread("myThread2");
+		MyThread myThread3 = new MyThread("myThread3");
+		myThread1.start();
+		myThread2.start();
+		myThread3.start();
+		System.out.println("언제 출력될까요?");
 	}
+}
 
+class MyThread extends Thread {
+	public MyThread(String szName) {
+		super(szName);
+	}
+	public void run() {//MyThread스레드를 start()호출해야지만, 자동실행되는 메서드 
+		for(int cnt=0;cnt<100;cnt++) {
+			System.out.print(this.getName() + " ");
+		}
+	}
 }
