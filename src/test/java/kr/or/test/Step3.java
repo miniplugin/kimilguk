5d11918faec321f41d8dc5f6d68faab3073d3122
User <User@DESKTOP-6223J9B>
2020-12-09 10:18:38 +0900

    2교시 중간 커밋

--- src/test/java/kr/or/test/Step3.java
+++ src/test/java/kr/or/test/Step3.java
@@ -22,22 +22,31 @@ public class Step3 {
 		//스위치 조건문으로 1~7까지 반환값을 한글로 변환(아래)
 		switch(intWeek) {//스위치로 조건을 판단, 개발자 if문, switch문을 사용할지 결정.
 		case 1:
-			today = Week.월;	break;//브레이크는 case문장을 빠져나가는 명령
+			today = Week.일;	break;//브레이크는 case문장을 빠져나가는 명령
 		case 2:
-			today = Week.화; break;//Week자료형에 바로접근하는 방법 Week.
+			today = Week.월; break;//Week자료형에 바로접근하는 방법 Week.
 		case 3:
-			today = Week.수; break;
+			today = Week.화; break;
 		case 4:
-			today = Week.목; break;
+			today = Week.수; break;
 		case 5:
-			today = Week.금; break;
+			today = Week.목; break;
 		case 6:
-			today = Week.토; break;
+			today = Week.금; break;
 		case 7:
-			today = Week.일; break;
+			today = Week.토; break;
 		}
 		
-		System.out.println("오늘 요일은: " + today + "요일 입니다.");
+		System.out.println("PC의 오늘 요일은: " + today + " 요일 입니다.");
+		
+		if(today == Week.수) {//이번엔 if조건문을 사용.
+			System.out.println("수요일에는 축구를 합니다.");
+		} else {
+			System.out.println("열심히 자바 공부를 합니다.");
+		}
+		
+		//Calendar클래스는 달력에서 날짜, 본인 컴퓨터 날짜라서 사용자가 날짜를 수정하면 원하는 결과가 안 나옵니다.
+		//이 때 해결책은 서버시간을 가져와서 출력하면, 사용자가 날짜를 수정 못하게 됩니다.
 	}
 
 }
