386c3cc4a141160778d7a51f3dfd24916b59b15b
User <User@DESKTOP-6223J9B>
2020-12-18 12:51:00 +0900

    PageVO클래스 생성 OK.

--- src/main/java/org/edu/vo/PageVO.java
+++ src/main/java/org/edu/vo/PageVO.java
@@ -23,33 +23,56 @@ public class PageVO {
 	private int startPage;//jsp화면에서 보여주는 페이징 리스트의 시작번호
 	private int endPage;//jsp화면에서 보여주는 페이징 리스트의 끝번호
 	//start페이지와 end페이지 변수가 필요한 이유?
-	//예) < 1 2 3 4 5 6 7 8 9 10 > 페이징 리스트의 시작1 과 끝10 값이 바뀌게 됩니다.
-	//예) < 11 12 13 14 15 16 17 18 19 20 > 시작 11 과 끝 20
+	
 	//검색에 필요한 변수 2개도 포함시켜서, 컨트롤러에서 매개변수 사용을 축소하게 됩니다.
 	private String search_type;//검색조건
 	private String search_keyword;//검색어
 	
 	//전체 클래스에서 [계산식]이 4개 필요합니다. 개발자가 만들어야 합니다.(아래)
+	//계산식4개로 반환되는 값은: startPage(11), endPage(20), prev(true), next(false)
+	//아래 메서드에서 사용되는 totalCount변수는 컨트롤러에서 쿼리문을 통해서 전송받습니다.
 	private void calcPage() {
-		//page변수는 현재 jsp에서 클릭한 페이지번호 예로 1 를 받아서 아래 계산식에서 사용
+		//page변수는 현재 jsp에서 클릭한 페이지번호 예로 11 를 받아서 아래 계산식에서 사용
 		//(int)형변환 : 2.1, 2.8 다 2로 반환이 됩니다.
 		//ceil메서드: 천장값을 반환   1.1 => 2, 2.3 => 3
 		//floor메서드: 바닥값을 반환 1.1 => 1, 2.3 => 2
-		//ceil(1/10) =>1.0 0.9 0.8 ...0.1 0.0 -0.1 -0.2 => 1
-		//ceil(1/10)*10 => 10페이지
+		//ceil(1/10) =>1.0 0.9 0.8...0.1 0.0 -0.1 -0.2 => 1
+		//2 1.9 1.8 ... 1.2 1.1 => 2
+		//ceil(11/10)*10 => 20페이지
 		int tempEnd = (int)(Math.ceil(
 				(page/(double)this.perPageNum)*this.perPageNum
 				));
 		//jsp에서 클릭한 페이지번호 예로 1 을 기준으로 끝 페이지를 계산한다(위)
+		//예) < 1 2 3 4 5 6 7 8 9 10(tempEnd) > 페이징 리스트의 시작1 과 끝10 값이 바뀌게 됩니다.
+		//예) < 11 12 13 14 15 16 17 18 19 20(tempEnd) > 시작 11 과 끝 20
 		this.startPage = (tempEnd - this.perPageNum) + 1;
-		//
+		//jsp에서 11을 클릭했을때 (20 - 10) + 1 = 11 스타트 페이지 값(위)
+		//(아래) 20 x 10 = 200 개의 레코드(회원[게시물])
+		//만약 회원[게시물] 195개 일 경우가 있습니다.
+		if(tempEnd*this.perPageNum > this.totalCount) {//경우 200>195
+			//클릭한 page번호로 계산된 게시물수가 실제게시물(totalCount)수보다 클때
+			this.endPage = (int)Math.ceil(
+					this.totalCount/(double)this.perPageNum
+					);// 195/10 => [20] 19.9 19.8 ... 19.5
+		} else {
+			//전체 회원[게시물]수가 195일때 page 1을 클릭한 경우 100 > 195
+			//결과가 195/10 => 20 잘못됨, 다음처럼 나와서 위 조건을 타면 않되고 else 문을 진입하게 됨.
+			this.endPage = tempEnd;//tempEnd가 10이니까 endPage가 10
+		}
+		//================== 여기까지가 endPage를 구하는 계산식 ==================
+		//아래는 prev, next 구하는 계산식
+		this.prev = (this.startPage != 1);//예, 스타트페이지 11 결과값은 true
+		//시작페이지가 1보다 크면 무조건 이전 페이지가 있다고 봄.(위)
+		this.next = (this.endPage*this.perPageNum < this.totalCount);
+		//20x10 < 195 결과는 false 이기 때문에 jsp에서  >표시가 않보이게 처리함.
+		//예) < 11 12 13 14 15 16 17 18 19 20(tempEnd)  시작 11 과 끝 20
 	}
 	
 	public Integer getPerPageNum() {
 		return perPageNum;
 	}
 	public void setPerPageNum(Integer perPageNum) {
-		perPageNum = 10;//강제로 1페이지당 보여줄 개수값을 10개로 지정
+		//perPageNum = 10;//강제로 1페이지당 보여줄 개수값을 10개로 지정
 		this.perPageNum = perPageNum;
 	}
 	public Integer getPage() {
@@ -59,6 +82,11 @@ public class PageVO {
 		this.page = page;
 	}
 	public Integer getStartNo() {
+		//DB쿼리에서 사용 결과값은 시작 인덱스번호(0)를 구하는 계산식(아래)
+		//계산식 = (jsp에서 클릭한페이지번호-1)*페이지당 보여지는 개수
+		//1페이지계산 10[1페이지당출력할개수]x(1[몇번째페이지번호]-1) = 0 1페이지일때
+		//2페이지계산 10x(2-1) = 10[계산결과나온 시작페이지번호] 2페이지일때
+		startNo = perPageNum*(this.page-1);//개발자가 추가한 계산식
 		return startNo;
 	}
 	public void setStartNo(Integer startNo) {
@@ -81,6 +109,8 @@ public class PageVO {
 	}
 	public void setTotalCount(int totalCount) {
 		this.totalCount = totalCount;
+		//totalCount변수값이 만들어지는 순간 calcPage메서드가 실행된면, 4개의 변수값 SET반환.
+		calcPage();//totalCount변수값이 필수로 필요한 메서드입니다.
 	}
 	public int getStartPage() {
 		return startPage;
