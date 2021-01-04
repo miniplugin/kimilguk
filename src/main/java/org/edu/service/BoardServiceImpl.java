1ce7390c120f216ba68a638f56030d5c3dcdb4e3
User <User@DESKTOP-6223J9B>
2021-01-04 12:07:02 +0900

    중간 커밋

--- src/main/java/org/edu/service/BoardServiceImpl.java
+++ src/main/java/org/edu/service/BoardServiceImpl.java
@@ -55,11 +55,12 @@ public class BoardServiceImpl implements IF_BoardService {
 		//첨부파일이 여러개일때 상황 대비
 		int index = 0;
 		String real_file_name = "";
-		if(save_file_names == null) { return; }//첨부파일이 없으면 진행 빠져나감.
+		if(save_file_names == null) { return; }//배열첨부파일이 없으면 진행 빠져나감.
 		for(String save_file_name:save_file_names) {//첨부파일 1개일때는 1번만 반복됩니다.
-			//if(save_file_name != null)
-			real_file_name = real_file_names[index];
-			boardDAO.insertAttach(save_file_name, real_file_name);
+			if(save_file_name != null) {//첨부파일배열에서 배열값이 있는경우만
+				real_file_name = real_file_names[index];
+				boardDAO.insertAttach(save_file_name, real_file_name);
+			}
 			index = index + 1;
 		}
 	}
@@ -84,9 +85,11 @@ public class BoardServiceImpl implements IF_BoardService {
 		int index = 0;
 		String real_file_name = "";
 		if(save_file_names == null) { return; }
-		for(String save_file_name:save_file_names) {//첨부파일 1개일때는 1번만 반복됩니다.
-			real_file_name = real_file_names[index];
-			boardDAO.updateAttach(save_file_name, real_file_name, bno);
+		for(String save_file_name:save_file_names) {//첨부파일 개수 만큼 반복됩니다.
+			if(save_file_name != null) {
+				real_file_name = real_file_names[index];
+				boardDAO.updateAttach(save_file_name, real_file_name, bno);
+			}
 			index = index + 1;
 		}
 	}
