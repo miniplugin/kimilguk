4cc8ba4250f36a084741562a907181b58a281301
User <User@DESKTOP-6223J9B>
2021-01-12 14:41:12 +0900

    5교시 커밋

--- src/main/java/org/edu/controller/LoginController.java
+++ src/main/java/org/edu/controller/LoginController.java
@@ -1,5 +1,6 @@
 package org.edu.controller;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,7 +20,13 @@ public class LoginController {
 	//사용자 홈페이지 로그인 접근 매핑
 	@RequestMapping(value="/login",method=RequestMethod.GET)
 	public String login() throws Exception{
-		
+		//BCrypt암호화 match 메서드으로 확인
+		String rawPassword = "user02";
+		BCryptPasswordEncoder bcryptPasswordEncoder = new BCryptPasswordEncoder(10);
+		String bcryptPassword = bcryptPasswordEncoder.encode("user02");//예, user02 -> 암호화 처리됨
+		//System.out.println(bcryptPassword);
+		System.out.println("2가지 스트링을 매칭 참,거짓: " + bcryptPasswordEncoder.matches(rawPassword, bcryptPassword));
+
 		return "home/login";
 	}
 }
