e09ca68d7085454bd252e016f4a76e4366cf70c9
User <User@DESKTOP-6223J9B>
2021-01-15 09:57:00 +0900

    1교시 커밋 입니다.

--- src/test/java/org/edu/test/DataSourceTest.java
+++ src/test/java/org/edu/test/DataSourceTest.java
@@ -16,6 +16,7 @@ import org.edu.vo.MemberVO;
 import org.edu.vo.PageVO;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -49,7 +50,7 @@ public class DataSourceTest {
 		Date primaryKey = new Date();
 		SimpleDateFormat newFormat = new SimpleDateFormat("yyyyMMddHHmmssSSS");
 		System.out.println("프라이머리키 : " + newFormat.format(primaryKey));
-		return "user_" + newFormat.format(primaryKey);
+		return "dummy_" + newFormat.format(primaryKey);
 	}
 	
 	@Test
@@ -91,15 +92,18 @@ public class DataSourceTest {
 		String memberIdKey = memberPrimaryKey();
 		memberVO.setUser_id(memberIdKey);
 		memberVO.setUser_name("사용자03");
-		//패스워드 암호화 처리(필수이지만, 스프링 시큐리티 할때 처리 예정)
-		memberVO.setUser_pw("1234");
+		//패스워드 암호화 처리(필수이지만, 스프링 시큐리티 엔코더처리 아래)
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		memberVO.setUser_pw(passwordEncoder.encode("1234"));
 		memberVO.setEmail("user03@abc.com");
 		memberVO.setPoint(100);
 		memberVO.setEnabled(true);
 		memberVO.setLevels("ROLE_USER");
 		Date reg_date = new Date();
 		memberVO.setReg_date(reg_date);//매퍼쿼리에서 처리로 대체
-		memberDAO.insertMember(memberVO);
+		for(int cnt=0;cnt<=100;cnt++) {//더미사용자 100명 입력
+			memberDAO.insertMember(memberVO);
+		}		
 	}
 	
 	@Test
