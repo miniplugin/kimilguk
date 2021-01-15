cf8da2968da1dd5c2ea04c551ebe8c0bdfcacd24
User <User@DESKTOP-6223J9B>
2021-01-15 12:38:18 +0900

    중간 커밋

--- src/test/java/org/edu/test/DataSourceTest.java
+++ src/test/java/org/edu/test/DataSourceTest.java
@@ -3,6 +3,7 @@ package org.edu.test;
 import static org.junit.Assert.*;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -153,13 +154,24 @@ public class DataSourceTest {
 		System.out.println(memberList.toString());
 	}
 	@Test
-	public void oldSelectTest() throws Exception {
-		Connection connection = dataSource.getConnection();
+	public void oldQueryTest() throws Exception {
+		//Connection connection = dataSource.getConnection();//root-context사용
+		Connection connection = null;
+		connection = DriverManager.getConnection("jdbc:hsqldb:file:c:/egov/workspace/embeded/hsql_file.db","sa","");
 		//직접 쿼리를 날립니다.(아래)
 		Statement stmt = connection.createStatement();
+		/* 인서트 쿼리실행(아래) 
+		for(int cnt=0;cnt<=100;cnt++) { //고전 방식으로 더미 데이터 입력하기(아래)
+		stmt.executeQuery("INSERT INTO tbl_board VALUES("
+				+ "(select count(*) from tbl_board)+1"
+				+ ",'강제 수정된 글입니다.', '수정 테스트 ', 'user00', now(),now(), 0, 0)");
+		}
+		*/
+		/* 셀렉트 쿼리실행(아래) */
 		ResultSet rs = stmt.executeQuery("select * from tbl_board");
-		System.out.println("제목\t\t내용\t\t작성자");
+		System.out.println("번호\t\t제목\t\t내용\t\t작성자");
 		while(rs.next()) {
+			System.out.print(rs.getString("bno"));
 			System.out.print(rs.getString("title"));
 			System.out.print(rs.getString("content"));
 			System.out.print(rs.getString("writer"));
