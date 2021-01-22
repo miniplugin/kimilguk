052877af8d9870455ec9230532fcd767a46666af
User <User@DESKTOP-6223J9B>
2021-01-22 09:28:27 +0900

    헤로쿠 배포용 설정OK

--- src/main/java/org/edu/controller/NaverLoginController.java
+++ src/main/java/org/edu/controller/NaverLoginController.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import javax.servlet.http.HttpSession;
 
 import org.edu.util.NaverLoginApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
@@ -18,6 +19,7 @@ import com.github.scribejava.core.model.Response;
 import com.github.scribejava.core.model.Verb;
 import com.github.scribejava.core.oauth.OAuth20Service;
 
+@PropertySource("classpath:properties/sns.properties")
 @Controller
 public class NaverLoginController {
 	/**
@@ -27,9 +29,15 @@ public class NaverLoginController {
 	 * redirect_uri:네이버 로그인 인증결과를 전달 받는 콜백URL
 	 * state: 네이버App이 생성한 토큰(네트웍에서 전송되는 자료단위-인증정보)의 상태
 	 */
-	private final static String CLIENT_ID = "osgm3bqYygYV49c1QPWl";
-	private final static String CLIENT_SECRET = "--------";
-	private final static String REDIRECT_URI = "http://127.0.0.1:8080/login_callback";
+	@Value("${SnsClientID}")
+	private String CLIENT_ID;
+	@Value("${SnsClientSecret}")
+	private String CLIENT_SECRET;
+	@Value("${SnsCallbackUri}")
+	private String REDIRECT_URI; 
+	//private final static String CLIENT_ID = "";
+	//private final static String CLIENT_SECRET = "-";
+	//private final static String REDIRECT_URI = "";
 	private final static String SESSION_STATE = "oauth_state";
 	/* 프로필 조회 API URL - 사용자이름+사용자이메일 */
 	private final static String PROFILE_API_URL = "https://openapi.naver.com/v1/nid/me";
