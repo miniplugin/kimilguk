43804762a7886aa8bc6263441da553767a542f66
User <User@DESKTOP-6223J9B>
2021-02-01 16:56:19 +0900

    JUnit 1초 증가부분 마무리 전 커밋

--- src/test/java/kr/or/test/HelloWorldApp.java
+++ src/test/java/kr/or/test/HelloWorldApp.java
@@ -1,8 +1,21 @@
 package kr.or.test;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class HelloWorldApp {
 
 	public static void main(String[] args) {
+		SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd HHmmss");
+		Date reg_date = new Date();
+		//boardVO.setBno(프라이머리키);
+		for(int cnt=0;cnt<=10;cnt++) {//더미게시물 100입력
+			Calendar cal = Calendar.getInstance();//+
+			cal.setTime(reg_date);//+
+			cal.add(Calendar.SECOND, cnt);//+ cnt초 더하기
+			System.out.println(format.format(cal.getTime()));
+		}
 		// 위 main()메서드는 자바프로그램의 실행 진입지점 입니다.
 		System.out.println("헬로우 월드!");
 		MemberVO memberVO = new MemberVO();
