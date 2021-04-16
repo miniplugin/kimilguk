fa46ab2d978938d27551f39420ddf59d9084aee9
boramcom@daum.net <boramcom@daum.net>
2021-04-16 14:48:14 +0900

    5교시 커밋 입니다.

--- src/main/java/org/edu/controller/JsonDataController.java
+++ src/main/java/org/edu/controller/JsonDataController.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,7 +28,7 @@ public class JsonDataController {
 	
 	//RestAPI서버 : 오픈챠트js에 투표한 값을 저장(업데이트 합니다.)
 	@RequestMapping(value="/chart/setdata", method=RequestMethod.POST)
-	public ResponseEntity<String> setData(ChartVO chartVO) {
+	public ResponseEntity<String> setData(@RequestBody ChartVO chartVO) {
 		ResponseEntity<String> entity = null;
 		try {
 			logger.debug("전송값 확인"+chartVO.toString());
